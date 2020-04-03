3590b70a5455d502efff181ce1052b2dc18f5a9e
Todd Walraven <cod4247@yahoo.com>
2020-04-03 12:30:55 -0700

    add using directive for entity framework

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
