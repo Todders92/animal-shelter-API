9538024ea7aa069fc742ccc33670a0b4a566e823
Todd Walraven <cod4247@yahoo.com>
2020-04-03 13:16:44 -0700

    update database to fix errors in seeded data

---
+++ AnimalShelter/Migrations/20200403201254_stuff.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class stuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                column: "Species",
+                value: "Lab Pitbull mix");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                column: "Species",
+                value: "Lab/Pitbull mix");
+        }
+    }
+}
